8b7ad0ea177ed5947a9f9ec6f188c12068004423
Kamlesh <itskamleshkumar.s@gmail.com>
2026-01-31 19:52:44 +0530

    two method templates created. fileNameCheck and importModuleCheck.

--- src/inputValidation.js
+++ src/inputValidation.js
@@ -2,4 +2,13 @@
 We validate the python script which has owlready2 code. For v0.0.1, minimal validation will be done.
 - python script check
 - import of owlready2 library check
-- include error/sucessful messages for these two checks. */
+- include error/sucessful messages for these two checks. Encapsulate the above in try-catch blocks.*/
+
+// function to check file as python script file.
+export function fileNameCheck() {
+
+}
+// function to use regex and check by parsing through the document for an "import owlready2"
+export function importModuleCheck() {
+
+}
